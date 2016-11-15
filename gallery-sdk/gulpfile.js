aae6174e530b10250ce3888f0a2d3c0190b6ee09
Nick Harris <nickha@microsoft.com>
2016-11-14 17:27:03 -0800

    remove auxdocs legacy /articles and /media directory

--- gallery-sdk/gulpfile.js
+++ gallery-sdk/gulpfile.js
@@ -13,24 +13,17 @@ var ncp = require('ncp');
 const sdkDir = __dirname;
 const generatedDir = path.resolve(sdkDir, 'generated');
 const templatesDir = path.resolve(sdkDir, 'templates');
-const articlesDir = path.resolve(process.cwd(), 'articles');
 const mediaSourceDir = path.resolve(sdkDir, 'media');
-const mediaAuxDocsLegacyDir = path.resolve(articlesDir, 'media');
 
 /**  
  * generates docs for gallery team
  */
 gulp.task('gallery', function () {
-    return gulpCommon.processDirectory(templatesDir, articlesDir, "^gallery.+\.md").then(function () {
-        if (!fs.existsSync(generatedDir)){
-            fs.mkdirSync(generatedDir);
-        }
-        return gulpCommon.processFile(path.resolve(templatesDir, "index-gallery.md"), generatedDir, {}, true);
-    }).then(function () {
-        //DPS does not support ref to media folder in the root, so until we deprecate auxdocs.azurewebsites.net media folder has to be dulplicated to /root        
-        return ncp(mediaSourceDir, mediaAuxDocsLegacyDir);
-    }).then(function () {
-        if(process.argv.indexOf("--verify") > 0) {
+    if (!fs.existsSync(generatedDir)) {
+        fs.mkdirSync(generatedDir);
+    }
+    return gulpCommon.processFile(path.resolve(templatesDir, "index-gallery.md"), generatedDir, {}, true).then(function () {
+        if (process.argv.indexOf("--verify") > 0) {
             return gulpCommon.checkLinks(path.resolve(generatedDir, "index-gallery.md"));
         }
     });
